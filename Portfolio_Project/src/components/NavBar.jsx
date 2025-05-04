16e50e8523b12fef9ef5b1d1338df784da56bb23
stepharinaa <stephanielplee@gmail.com>
2025-05-04 15:58:40 +0100

    add navbar

---
+++ Portfolio_Project/src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import { navLinks } from "../constants"
+
+
+const NavBar = () => {
+  return (
+    <header className="navbar">
+        <div className="inner">
+            <a className="logo" href="#hero">
+                Steph Lee
+            </a>
+
+            <nav className="desktop">
+                <ul>
+                    {navLinks.map(({link, name}) => (
+                        <li key={name} className="group">
+                            <a href={link}>
+                                <span>{name}</span>
+                                <span className="underline"/>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+
+            <a href="#contact" className='contact-btn group'> 
+                <div className='inner'>
+                    <span>Contact Me</span>
+                </div>
+            </a>
+        </div>
+    </header>
+  )
+}
+
+export default NavBar
